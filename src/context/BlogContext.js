8f98810cabac4904f35372e0642cebdbf7a6bd47
Rumit Tuladhar <rumitwashere@gmail.com>
2020-07-14 17:19:41 +0930

    useReeduceer template to add blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch(action.type){
+        case 'add_blogpost':
+            return [...state, {title: `Blog Post #${state.length+1}`}];
+        default:
+            return state;
+    }
+};
+
 export const BlogProvider = ({children}) => {
-    const blogPost = [
-        { title: 'Blog Post #1'},
-        { title: 'Blog Post #2'}
-    ];
+    const [blogPosts,dispatch] = useReducer(blogReducer,[]);
+
+    const addBlogPost = () => {
+        dispatch({ type: 'add_blogpost'});
+    }
+
+
     return (
-        <BlogContext.Provider value={blogPost}>
+        <BlogContext.Provider value={{data: blogPosts,addBlogPost}}>
             {children}
         </BlogContext.Provider>
     )
