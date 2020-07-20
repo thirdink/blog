08fe8f23889ae879cf4d64cb5ff712e0b2a738c7
Rumit Tuladhar <rumitwashere@gmail.com>
2020-07-20 15:12:22 +0930

    refactored with context and use reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,6 @@
-import React,{useReducer} from 'react';
+import createDataContext from'./createDataContext';
 
-const BlogContext = React.createContext();
+
 
 const blogReducer = (state, action) => {
     switch(action.type){
@@ -11,19 +11,16 @@ const blogReducer = (state, action) => {
     }
 };
 
-export const BlogProvider = ({children}) => {
-    const [blogPosts,dispatch] = useReducer(blogReducer,[]);
-
-    const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+    return ()=>{
         dispatch({ type: 'add_blogpost'});
     }
-
-
-    return (
-        <BlogContext.Provider value={{data: blogPosts,addBlogPost}}>
-            {children}
-        </BlogContext.Provider>
-    )
 }
 
-export default BlogContext;
+
+
+export const {Context,Provider} = createDataContext(
+    blogReducer,
+    {addBlogPost},
+    []
+);
